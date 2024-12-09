f8fa3a60bfaef319c122be06de455c62d18381ba
efe <victorefe5@gmail.com>
2024-12-09 20:30:00 +0100

    Dashboard implemented and working fine

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'django_select2',
+    "dashboard",
     "core",
     "contacts",
     "django_extensions",
