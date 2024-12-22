5a3cb1fd6acdbf9fc26e57135bd32196c2677980
efe <victorefe5@gmail.com>
2024-12-22 18:46:30 +0100

    problem code

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-il*1%z=6!0#^suy_8p@eys$3vpq=nuhb$n6t_&%bo_9@dw(zoj"
-
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -149,9 +148,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# # If you're using Django Compressor
+# COMPRESS_ROOT = BASE_DIR / 'static'
+# COMPRESS_ENABLED = True
+# STATICFILES_FINDERS = ('compressor.finders.CompressorFinder', )
+
+
+
+# Path to your static directory for Tailwind
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 
--- crm/tailwind.config.js
+++ crm/tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    // "./templates/**/*.html",        // All HTML files in templates directory
+    // "./**/*.html",                  // Catch any stray HTML files in the project
+    // "./static/**/*.js",             // Include JS files inside static (if applicable)
+    // "./crm/**/*.html",              // If templates are inside app directories
+
+    './crm/**/templates/**/*.html',
+    './templates/**/*.html', // Adjust this path to match where your Django templates are stored.
+    './static/**/*.js',      // Include your static JS files if you're using Tailwind classes in JavaScript.
+    './static/**/*.css',     // Optional: Include static CSS if Tailwind classes are dynamically generated there.
+    "./crm/**/*.html",              // If templates are inside app directories
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+  ],
+};
